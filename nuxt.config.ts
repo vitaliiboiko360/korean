ca03cc31d826ce70b6b6ec14254ef9e79d7fd639
vitaliiboiko360 <vukelan@gmail.com>
2026-08-11 20:33:59 +0300

    add custom css

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,7 +3,7 @@ export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
   devtools: { enabled: true },
   modules: ['nuxt-quasar-ui', '@vue-player/nuxt'],
-  css: ['assets/app.scss'],
+  css: ['assets/app.scss', 'assets/custom.css'],
   vite: {
     assetsInclude: ['**/*.mp4'],
   },
