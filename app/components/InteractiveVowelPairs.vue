ff594b5e01577a5e017d46b4c751f82246a3af1e
vitaliiboiko360 <vukelan@gmail.com>
2026-08-13 21:50:30 +0300

    add interactive audio to syllables

---
+++ app/components/InteractiveVowelPairs.vue
@@ -0,0 +1,28 @@
+<script setup>
+import hangeulCommonVowelSyllables from '~/assets/hangeul_common_vowel_syllables.json';
+const { vowel = 'ㄴ' } = defineProps(['vowel']);
+
+const vowelPairsInfo = hangeulCommonVowelSyllables[vowel];
+
+const audioRef = useTemplateRef('audioRef');
+
+const getAudioUrl = () =>
+  `/vowel_pairs/${vowelPairsInfo.characterName}/${vowelPairsInfo.fileName}`;
+
+onMounted(() => {
+  console.log(JSON.stringify(vowelPairsInfo));
+});
+</script>
+
+<template>
+  <div>
+    <audio ref="audioRef" :src="() => getAudioUrl()"></audio>
+    <CommonVowelPairs :letter="vowel" />
+  </div>
+</template>
+
+<style module>
+.outerDiv {
+  display: flex;
+}
+</style>
